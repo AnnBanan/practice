f23d1d5bf0900f08253f0a848d0c65499084a6f8
Anna Lloyd <bal100@pitt.edu>
2019-10-25 15:03:02 -0400

    framed data

--- prac.R
+++ prac.R
@@ -1,3 +1,5 @@
 tigerData <- read.csv(url("http://whitlockschluter.zoology.ubc.ca/wp-content/data/chapter02/chap02e2aDeathsFromTigers.csv"))
 
 tigerTable <- sort(table(tigerData$activity), decreasing = TRUE)
+
+data.frame(Frequency = tigerTable)
