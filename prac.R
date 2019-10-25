9bf076fd25f0a9a7806fc762caacf2dc9c8ef649
Anna Lloyd <bal100@pitt.edu>
2019-10-25 15:02:02 -0400

    created tiger table

--- prac.R
+++ prac.R
@@ -1,1 +1,3 @@
 tigerData <- read.csv(url("http://whitlockschluter.zoology.ubc.ca/wp-content/data/chapter02/chap02e2aDeathsFromTigers.csv"))
+
+tigerTable <- sort(table(tigerData$activity), decreasing = TRUE)
